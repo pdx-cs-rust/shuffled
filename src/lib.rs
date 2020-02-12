a043f938e6cf4dc7ae1f8fbf7fb685d5d052dbce
Bart Massey <bart@cs.pdx.edu>
2020-02-12 14:44:12 -0800

    commented lib; cleaned up iterator

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,27 @@
+//! Iterator for lazily producing references to elements of
+//! a data structure in shuffled order. This consumes random
+//! numbers only as elements are produced.
+
 use rand::{rngs::ThreadRng, Rng};
 
+/// Iterator for lazily producing references to
+/// elements.
 pub struct ShuffledIter<'a, T, R: Rng> {
     rng: R,
     state: Vec<&'a T>,
 }
 
+/// Extension trait for lazily producing elements in
+/// shuffled order from a given collection.
 pub trait Shuffled<'a, T: 'a> {
+    /// Get an iterator that lazily produces elements in
+    /// shuffled order from a given collection using the
+    /// given `Rng`.
     fn shuffled_by<R: Rng>(&'a self, rng: R) -> ShuffledIter<'a, T, R>;
 
+    /// Get an iterator that lazily produces elements in
+    /// shuffled order from a given collection using
+    /// `rand::ThreadRng`.
     fn shuffled(&'a self) -> ShuffledIter<'a, T, ThreadRng> {
         self.shuffled_by(rand::thread_rng())
     }
@@ -20,16 +34,21 @@ impl<'a, T: 'a> Shuffled<'a, T> for [T] {
     }
 }
 
+/// Iterator for lazily producing references to elements of
+/// a data structure in shuffled order.
 impl<'a, T: 'a, R: Rng> Iterator for ShuffledIter<'a, T, R> {
     type Item = &'a T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let nstate = self.state.len();
-        if nstate == 0 {
-            return None;
+        let last = self.state.pop()?;
+        let top = self.state.len();
+        let j = self.rng.gen_range(0, top + 1);
+        if j < top {
+            let result = self.state[j];
+            self.state[j] = last;
+            Some(result)
+        } else {
+            Some(last)
         }
-        let j = self.rng.gen_range(0, nstate);
-        self.state.swap(nstate - 1, j);
-        self.state.pop()
     }
 }
